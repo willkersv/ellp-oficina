8aaf2974ef3199b2e0f080d0ec6ad0d86b318ba9
willkersv <willkersv@gmail.com>
2024-11-16 10:55:07 -0300

    testes random

--- backend/src/test/java/com/ellp/certificado/CertificadoApplicationTests.java
+++ backend/src/test/java/com/ellp/certificado/CertificadoApplicationTests.java
@@ -1,13 +1,38 @@
 package com.ellp.certificado;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
-class CertificadoApplicationTests {
+import org.junit.jupiter.api.Test;
+
+public class CertificadoApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+    void testIfTrueCondition() {
+        int a = 5;
+        int b = 10;
+        assertTrue(a < b, "A deve ser menor que B");
+    }
+
+    @Test
+    void testIfFalseCondition() {
+        int a = 10;
+        int b = 5;
+        assertFalse(a < b, "A não deve ser menor que B");
+    }
+
+    @Test
+    void testEquality() {
+        String str1 = "Hello";
+        String str2 = "Hello";
+        assertTrue(str1.equals(str2), "As strings devem ser iguais");
+    }
+
+    @Test
+    void testInequality() {
+        String str1 = "Hello";
+        String str2 = "World";
+        assertFalse(str1.equals(str2), "As strings não devem ser iguais");
+    }
 
 }
